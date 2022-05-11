e5f37815b854105e0338aa4da7ca2f49818ebe51
王瑞 <wangrui5@songguo7.com>
2022-05-11 14:16:30 +0800

    add cache

---
+++ cache.go
@@ -0,0 +1,44 @@
+package GeeCache
+
+import (
+	"geecache/lru"
+	"sync"
+)
+
+type cache struct {
+	mu sync.Mutex
+	lru *lru.Cache
+	cacheBytes int64
+}
+
+// Add 添加缓存
+func (c *cache) Add(key string, value ByteView)  {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if c.lru == nil {
+		c.lru = lru.New(c.cacheBytes, nil)
+	}
+
+	c.lru.Add(key, value)
+}
+
+// Get 获取缓存
+func (c *cache) Get(key string) (value ByteView, ok bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if c.lru == nil {
+		return
+	}
+
+	v, ok := c.lru.Get(key)
+	if ok {
+		return v.(ByteView), ok
+	}
+
+	return
+}
+
+
+
